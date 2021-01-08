b93ecc655ca54f9fbe88e134e3418250e335dc7f
SmallPotatoCZ <sp.zhangchao@gmail.com>
2021-01-08 11:37:55 +0800

    添加类的基本知识

--- src/4/4.7/MyApp.java
+++ src/4/4.7/MyApp.java
@@ -1,5 +1,16 @@
+
+/**
+ * @version 1.0.1 - 2021-01-08 10:49:44
+ * @author Esily Zhang 
+ * @see <a href="http://www.esliy.cn">Esily Java Doc</a>
+ */
 import cn.esliy.horstmann.*;
 
+/**
+ * A {@code Card} object represent a playing card, such as "Queen of Hearts". A
+ * card has a suit (Diamond, Heart, Spacde or Club) and a value (1 = Ace,
+ * 2...10, 11 = Jack, 12 = Queen, 13 = King)
+ */
 public class MyApp {
   public static void main(String[] args) {
     var stuff1 = new Employee("Esily", 10, 2020, 9, 10);
--- src/4/4.7/cn/esliy/horstmann/Employee.java
+++ src/4/4.7/cn/esliy/horstmann/Employee.java
@@ -3,8 +3,17 @@ package cn.esliy.horstmann;
 import java.time.*;
 
 public class Employee {
+  /**
+   * The name of the employee.
+   */
   private String name;
+  /**
+   * the salary of employee.
+   */
   private double salary;
+  /**
+   * the hire date of employee.
+   */
   private LocalDate hireDay;
 
   public Employee(String name, double salary, int year, int month, int day) {
@@ -25,6 +34,13 @@ public class Employee {
     return hireDay;
   }
 
+  /**
+   * Raise the salary of an employee.
+   * 
+   * @param byPercent the percentage by which to raise the salary (eg., 10 means
+   *                  10%)
+   * @return the amount of salary
+   */
   public void raiseSalary(double byPercent) {
     double raise = salary * byPercent / 100;
     salary += raise;
